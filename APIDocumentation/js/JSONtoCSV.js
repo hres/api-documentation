0c9b0d54712bcd3c2e5d40e0f0e35e3552c50a2b
ahasangit <asif.hasan@canada.ca>
2017-03-08 15:42:00 -0500

    Added support for IE 10+ for JSON to CSV download

--- APIDocumentation/js/JSONtoCSV.js
+++ APIDocumentation/js/JSONtoCSV.js
@@ -1,12 +1,14 @@
-function JSONtoCSV(fileName, url)
-{
-    console.log("Test");
+function JSONtoCSV(fileName, url) {
+
+
 
     var json = $.getJSON(url, function (json) {
 
         var row = "";
         var csv = '';
+
         var arrData = typeof json != 'object' ? JSON.parse(json) : json;
+
 
         //extract the first row of data as headers
         for (var index in arrData[0]) {
@@ -18,11 +20,10 @@ function JSONtoCSV(fileName, url)
         csv += row + '\r\n';
 
         for (var i = 0; i < arrData.length; i++) {
-            var row = "";   
+            var row = "";
             for (var index in arrData[i]) {
                 row += '"' + arrData[i][index] + '",';          //create rows from the JSON data objects
             }
-
             row.slice(0, row.length);
 
             csv += row + '\r\n';                               //add it to the CSV string
@@ -32,70 +33,26 @@ function JSONtoCSV(fileName, url)
 
         var csvUrl = window.URL.createObjectURL(blob);
 
-        $("<a />", {
-            "download": fileName + '.csv',
-            "href": csvUrl,
-        }).appendTo("body")     //create, append and remove a download link
-                        .click(function () {
-                            $(this).remove()
-                        })[0].click()
-    });
-
-    //});
-
-}
-
-
-/*
-   var xhr = createCORS('GET', url);
-
-   if (!xhr) {
-       throw new Error('CORS not supported. Unable to download CSV file');
-   }
-
-   xhr.onload = function () {
-       var responseText = xhr.responseText;
-       console.log(responseText);
-   }
-
-   xhr.onerror = function () {
-       throw new Error('there was an error with the XHttpRequest');
-   }
-   */
-//xhr.send(function () {
-/*
-$.ajax({
-
-    type: 'GET',
-    url: url,
-    contentType: 'text/csv',
-    xhrFields:
+        if ((!!document.documentMode == true)) //IF IE > 10
         {
-            
+            navigator.msSaveBlob(blob, fileName + '.csv');
         }
 
-});
-*/
+        else if (navigator.userAgent.indexOf("Chrome") != -1 || navigator.userAgent.indexOf("Firefox") != -1 || navigator.userAgent.indexOf("Opera") != -1 || navigator.userAgent.indexOf('OPR') != -1) {
 
-/*
-function createCORS(method, url)
-{
-    var xhr = new XMLHttpRequest();
+            $("<a />", {
+                "download": fileName + '.csv',
+                "href": csvUrl,
+            }).appendTo("body")     //create, append and remove a download link
+                            .click(function (e) {
+                                $(this).remove()
+                            })[0].click()
+        }
+        else {
+            alert("Your Browser does not support that feature.");
+        }
+    });
 
-    if ("withCredentials" in xhr) 
-    {
-        xhr.open(method, url, true);
-    }
-    else if(typeof XDomainRequest != "undefined")
-    {
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    }
-    else
-    {
-        xhr = null;
-    }
- 
-    return xhr;
- */
+
+}
 
