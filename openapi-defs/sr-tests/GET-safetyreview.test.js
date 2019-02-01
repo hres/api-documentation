adf76c56a70c39c505aaa32fc94c70741071216d
alexgagnon <asgagnon@hotmail.com>
2019-02-01 16:21:01 -0500

    added initial perf tests

---
+++ openapi-defs/sr-tests/GET-safetyreview.test.js
@@ -0,0 +1,40 @@
+
+import http from 'k6/http';
+import {check} from 'k6';
+import {Rate} from 'k6/metrics';
+
+const user_key = __ENV.USER_KEY;
+const method = __ENV.METHOD || 'GET';
+const url = `https://summary-report-hc-sc-apicast-staging.dev.api.canada.ca/api/summary-report/api//safetyreview?user_key=${user_key}`;
+const payload = undefined;
+const parameters = undefined;
+
+const myFailRate = new Rate('failed requests');
+
+export const options = {
+  thresholds: {
+    'failed requests': [
+      {
+        threshold: 'rate<0.1',
+        abortOnFail: true,
+        delayAbortEval: '10s'
+      }
+    ]
+  }
+};
+
+export default function() {
+  const response = http.request(
+    method,
+    url,
+    payload,
+    parameters
+  );
+
+  myFailRate.add(response.status !== 200);
+
+  check(response, {
+    'status 200': r => r.status === 200,
+    // ...other checks
+  });
+}
