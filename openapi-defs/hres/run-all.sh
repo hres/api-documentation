bc35da8d4e9bc472aae330444744433d2321412e
alexgagnon <asgagnon@hotmail.com>
2019-03-20 10:50:24 -0400

    test resuilts

--- openapi-defs/hres/run-all.sh
+++ openapi-defs/hres/run-all.sh
@@ -12,33 +12,34 @@ DATE=$(date -I)
 EXEC=/home/alex/dev/api-tools/packages/cli/cli.js
 
 declare -a APIS=(\
-# "dpd" \
-# "lnhpd" \
-# "mdall" \
-# "cnf" \
-# "cta" \
-# "cvp" \
-# "noc" \
+"dpd" \
+"lnhpd" \
+"mdall" \
+"noc" \
 "sr"\
+"cnf" \
+"cta" \
+"cvp" \
 )
 
 declare -a FOLDERS=(\
 "avg" \
-# "1",
-# "50",
-# "200"
+"1",
+"10",
+"50",
+"200"
 )
 
 for API in "${APIS[@]}"; do
    $EXEC test -s tests/$API/**/*.test.js -o tests/$API/$LOCATION/$DATE/avg -f file -w 1 --average 10
 
-  #  $EXEC test -s tests/$API/**/*.test.js -o tests/$API/$LOCATION/$DATE/1 --vus 1 --duration 1m --format file -F
+   $EXEC test -s tests/$API/**/*.test.js -o tests/$API/$LOCATION/$DATE/1 --vus 1 --duration 1m --format file -F
 
-  #  $EXEC test -s tests/$API/**/*.test.js -o tests/$API/$LOCATION/$DATE/50 --vus 50 --duration 1m --format file -F
+   $EXEC test -s tests/$API/**/*.test.js -o tests/$API/$LOCATION/$DATE/10 --vus 10 --duration 1m --format file -F
 
-  #  $EXEC test -s tests/$API/**/*.test.js -o tests/$API/$LOCATION/$DATE/100 --vus 100 --duration 1m --format file -F
+   $EXEC test -s tests/$API/**/*.test.js -o tests/$API/$LOCATION/$DATE/50 --vus 50 --duration 1m --format file -F
 
-  #  $EXEC test -s tests/$API/**/*.test.js -o tests/$API/$LOCATION/$DATE/200 --vus 200 --duration 1m --format file -F
+   $EXEC test -s tests/$API/**/*.test.js -o tests/$API/$LOCATION/$DATE/200 --vus 200 --duration 1m --format file -F
 
   for FOLDER in "${FOLDERS[@]}"; do
     $EXEC report -s "tests/$API/$LOCATION/$DATE/*" -o tests/$API/$LOCATION/$DATE/report.json
